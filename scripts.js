36fcd18ea17c113c8f4d6560c63046f668cb87f1
Thandolwethu Mangesana <Mangesanathandolwethu@gmail.com>
2025-10-26 00:57:35 +0200

    Refactor filter section and initialize modal
    Updated the filter section in Index.html for improved structure and accessibility. Added modal information sections and podcast grid container. In scripts.js, initialized the modal with genre mapping and podcast data, and set up references to new DOM elements.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
 import { podcasts } from './scripts.js';
+import { modal  from } './modals.js';
+
+const grid = document.getElementById('podcast-grid');
+const genreFilter = document.getElementById('genre');
+
+// Map genre IDs to titles
+const genreMap = {};
+genres.forEach(g => {
+    genreMap[g.id] = g.title;
+});
+
+// setup modal
+const podcastModal = new modal('podcast-modal', {genreMap, podcasts});
+
+
